2d620edd07d5eeabf67ff8e5584063bdf6fd85f2
SolaiTechLab <Solairaj.Mariappan>
2023-02-07 16:19:09 +0530

    Added Application and Test Projects in existing skeleton

---
+++ MyCartApplication.Test/ProductServiceTest.cs
@@ -0,0 +1,11 @@
+namespace MyCartApplication.Test
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        [TestMethod]
+        public void GetAllProducts()
+        {
+        }
+    }
+}
---
+++ MyCartApplication/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using MyCartDomain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCartApplication
+{
+    public class ProductService
+    {
+        public List<Product> GetProducts(int CategoryId)
+        {
+            List<Product> products = new List<Product>();
+            return products;
+        }
+    }
+}
